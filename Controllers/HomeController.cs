33e73a62d98e4b117443472712463e545364d3a5
Torianna Hudson <toriannahudson@quickenloans.comclear>
2021-10-04 21:16:34 -0400

    final touches

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Lab13_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab13_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetForm()
+        {
+            return View();
+        }
+
+        public IActionResult UseForm(string nametext, string namenew, string email, string password)
+        {
+            if (nametext==null || namenew==null || email==null|| password==null)
+            {
+                return Content("Please fill in all fields");
+            }
+            
+            else
+            {
+                return Content($"{nametext} - {namenew} - {email} - {password}");
+            }
+
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab13_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
